6682a373b742f4a17e585f1018cb25b271cc9841
Mohammed Shamal <Mohammed.Shamal@novigo.com>
2025-05-22 13:36:34 +0530

    update server.js to use hardcoded token for API requests; modify swagger.yaml to clarify server descriptions

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 //   try {
 //     const filePath = req.file.path;
 //     const fileStream = fs.createReadStream(filePath);
-//    const TOKEN = process.env.TOKEN;
+//     const TOKEN = "dapi920f192e4012fa389f31d19a3350dab1-3";
 //     const response = await axios.put(
 //       "https://adb-3942989410469201.1.azuredatabricks.net//api/2.0/fs/files/Volumes/datalink/lineagedemo/user_input/user_input7.mp3?overwrite=true",
 //       fileStream,
@@ -49,7 +49,7 @@ app.post("/upload", upload.single("file"), async (req, res) => {
       const timestamp = new Date().toISOString().replace(/[:.]/g, "-"); // e.g., 2025-05-21T15-22-30-123Z
       const newFileName = `user_input_${timestamp}.mp3`;
   
-     const TOKEN = process.env.TOKEN;
+      const TOKEN = "dapi920f192e4012fa389f31d19a3350dab1-3";
       const uploadUrl = `https://adb-3942989410469201.1.azuredatabricks.net/api/2.0/fs/files/Volumes/datalink/lineagedemo/user_input/${newFileName}?overwrite=true`;
   
       const response = await axios.put(
@@ -84,7 +84,7 @@ app.head("/file-metadata", async (req, res) => {
 
   PATH =
     "https://adb-3942989410469201.1.azuredatabricks.net//api/2.0/fs/files/Volumes/datalink/lineagedemo/output_audio/output.mp3";
- const TOKEN = process.env.TOKEN;
+  const TOKEN = "dapi920f192e4012fa389f31d19a3350dab1-3";
   try {
     const response = await axios.head(`${PATH}`, {
       headers: {
@@ -109,7 +109,7 @@ app.head("/file-metadata", async (req, res) => {
 app.get("/audio-binary", async (req, res) => {
   const AUDIO_URL =
     "https://adb-3942989410469201.1.azuredatabricks.net//api/2.0/fs/files/Volumes/datalink/lineagedemo/ouput_audio/output.mp3";
- const TOKEN = process.env.TOKEN;
+  const TOKEN = "dapi920f192e4012fa389f31d19a3350dab1-3";
 
   try {
     const response = await axios.get(AUDIO_URL, {
